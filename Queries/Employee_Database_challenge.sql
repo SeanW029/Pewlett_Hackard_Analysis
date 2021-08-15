695ff8b5e4551e48a720bb2492f6549adb5fc3aa
unknown <seanwang029@gmail.com>
2021-08-14 20:24:27 -0400

    deliverable 1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,83 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+CREATE TABLE departments (
+  dept_no VARCHAR(4) NOT NULL,
+  dept_name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (dept_no),
+  UNIQUE (dept_name)
+);
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+CREATE TABLE employees (
+  emp_no INT NOT NULL,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  gender VARCHAR NOT NULL,
+  hire_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+
+SELECT e.emp_no
+, e.first_name
+, e.last_name
+, t.title
+, t.from_date
+, t.to_date
+INTO retirement_title
+FROM employees AS e
+LEFT JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+
+
+select *
+From retirement_title
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, 
+first_name, 
+last_name, 
+title
+INTO unique_titles
+FROM retirement_title 
+ORDER BY emp_no, to_date DESC
+
+select *
+From unique_titles
+
+SELECT COUNT(title) count, title
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC
+
+
+
+
